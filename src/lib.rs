41ef036f9cfde8a87a4bfe239bd41669cbeb9b1a
eugene <eugenebokhan@icloud.com>
2025-09-10 13:15:29 +0400

    remove old stuff

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,3 @@
 pub use objc2;
 pub use objc2_foundation;
 pub use block2;
-
-// Public sub-modules
-pub mod prelude;
-mod device;
-mod command_queue;
-mod command_buffer;
-mod buffer;
-pub mod command_buffer_descriptor;
-
-// Public re-exports for ergonomic `use metal_rs::*;`
-pub use device::Device;
-pub use command_queue::CommandQueue;
-pub use command_buffer::{CommandBuffer, CommandBufferStatus, CommandBufferError};
-pub use buffer::{Buffer, ResourceOptions as BufferResourceOptions};
-pub use command_buffer_descriptor::{CommandBufferDescriptor, CommandBufferErrorOption};
